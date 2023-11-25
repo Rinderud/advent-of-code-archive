2ab51d6ab974689a8efc1d86b70fda35b9c2350b
Jacob Rinderud <jacobrinderud@gmail.com>
2023-11-25 17:28:19 +0100

    Part 1 complete

--- 2022/src/1/main.c
+++ 2022/src/1/main.c
@@ -1,8 +1,38 @@
 #include <stdio.h>
-
+#include <ctype.h>
 
 int main(void)
 {
-    printf("I did it!");
+    unsigned int max, elf, snack;
+    int c;
+    max = 0;
+    elf = 0;
+    snack = 0;
+
+    while ((c = getchar()) != EOF)
+    {
+        if (isdigit(c))
+        {
+            snack = snack * 10 + (c - '0');
+        }
+        else if (c == '\n')
+        {
+            if (snack == 0)
+            {
+                elf = 0;
+            }
+
+            elf += snack;
+            snack = 0;
+
+            if (elf > max)
+            {
+                max = elf;
+            }
+        }
+    }
+
+    printf("%d", max);
+
     return 0;
 }
