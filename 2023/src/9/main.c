203c26eb888c7b25483c8e5e2232358d22659cfb
Jacob Rinderud <jacobrinderud@gmail.com>
2024-01-07 13:22:50 +0100

    almost there

--- 2023/src/9/main.c
+++ 2023/src/9/main.c
@@ -22,7 +22,7 @@ size_char_t read_numbers(long unsigned *nums, size_t N)
         switch (c)
         {
         case EOF:
-            printf("EOF\n");
+            // printf("EOF\n");
             c = '@';
         case '\n':
         case ' ':
@@ -41,32 +41,93 @@ size_char_t read_numbers(long unsigned *nums, size_t N)
         }
     }
 
+    /*
     if (i > 0)
     {
         printf("last num = %lu\n", nums[i - 1]);
     }
+    */
 
     size_char_t ret = {i, c};
     return ret;
 }
 
+long unsigned sum(long unsigned *nums, size_t cols)
+{
+    long unsigned sum = 0;
+    for (size_t i = 0; i < cols; i++)
+    {
+        sum += nums[i];
+    }
+    return sum;
+}
+
+long unsigned recur(long unsigned *nums, size_t cols)
+{
+    size_t i;
+    /*for (i = 0; i < cols; i++)
+    {
+        printf("%lu ", nums[i]);
+    }
+    printf("\n");
+*/
+    if (cols <= 1 || sum(nums, cols) == 0)
+    {
+        // printf("returning\n\n");
+        return 0;
+    }
+
+    long unsigned *newrow;
+    long unsigned a, b, diff;
+
+    newrow = calloc((cols - 1), sizeof(*newrow));
+
+    a = nums[0];
+    for (i = 1; i < cols; i++)
+    {
+        b = a;
+        a = nums[i];
+        diff = a - b;
+        // printf("%lu - %lu = %lu\n", a, b, diff);
+
+        newrow[i - 1] = diff;
+    }
+
+    long unsigned newdiff, newnewdiff;
+    newdiff = recur(newrow, cols - 1);
+    newnewdiff = newdiff + nums[cols - 1];
+
+    //printf("%lu + %lu = %lu\n", nums[cols - 1], newdiff, newnewdiff);
+
+    free(newrow);
+    return newnewdiff;
+}
+
 int main(void)
 {
-    size_t cols;
-    size_t N = 25;
+    size_t cols, N, i;
+    long unsigned sum, newdiff;
     long unsigned *nums;
 
+    N = 25;
     nums = calloc(N, sizeof(*nums));
 
     size_char_t ret = {0, '?'};
+    sum = 0;
+    i = 0;
     while (ret.c != '@')
     {
         ret = read_numbers(nums, N);
         cols = ret.i;
-        printf("i:%zu c:%c\n", ret.i, ret.c);
+        // printf("%lu\n", nums[cols-1]);
+
+        newdiff = recur(nums, cols);
+        printf("%zu: %lu\n",i, newdiff);
+        sum += newdiff;
+        i++;
     }
 
-    printf("Read %zu numbers\n", cols);
+    printf("total = %lu", sum);
 
     //! Frees
     free(nums);
