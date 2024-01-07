8f57149ec4be27ae24eb09f456d103edc8383794
Jacob Rinderud <jacobrinderud@gmail.com>
2024-01-07 14:18:34 +0100

    documentation

--- 2023/src/9/main.c
+++ 2023/src/9/main.c
@@ -8,6 +8,10 @@ typedef struct
     char c;
 } size_char_t;
 
+/**
+ * Returns a size_char struct with the last char read and the number of read numbers.
+ * The read numbers are added to the nums array.
+*/
 size_char_t read_numbers(long signed *nums, size_t N)
 {
     size_t i;
@@ -27,7 +31,7 @@ size_char_t read_numbers(long signed *nums, size_t N)
             c = '@';
         case '\n':
         case ' ':
-            nums[i] = new*sign;
+            nums[i] = new *sign;
             i++;
             new = 0;
             sign = 1;
@@ -50,7 +54,10 @@ size_char_t read_numbers(long signed *nums, size_t N)
     return ret;
 }
 
-long signed allzro(long signed *nums, size_t cols)
+/**
+ * Return the manhattan distance of the nums arrays first cols elements.
+*/
+long signed mandist(long signed *nums, size_t cols)
 {
     long signed sum = 0;
     for (size_t i = 0; i < cols; i++)
@@ -60,9 +67,12 @@ long signed allzro(long signed *nums, size_t cols)
     return sum;
 }
 
+/**
+ * Recursive function that returns the new predicted value as per the AOC task.
+*/
 long signed recur(long signed *nums, size_t cols)
 {
-    if (cols <= 1 || allzro(nums, cols) == 0)
+    if (cols <= 1 || mandist(nums, cols) == 0)
     {
         return 0;
     }
@@ -91,7 +101,7 @@ long signed recur(long signed *nums, size_t cols)
 
 int main(void)
 {
-    size_t cols, N, i;
+    size_t N, i;
     long signed sum, newdiff;
     long signed *nums;
     size_char_t ret;
@@ -104,10 +114,8 @@ int main(void)
     i = 0;
     while (ret.c != '@')
     {
-        ret = read_numbers(nums, N);
-        cols = ret.i;
-
-        newdiff = recur(nums, cols);
+        ret = read_numbers(nums, N); // Read in a row
+        newdiff = recur(nums, ret.i);
         printf("%zu: %ld\n", i, newdiff);
         sum += newdiff;
         i++;
