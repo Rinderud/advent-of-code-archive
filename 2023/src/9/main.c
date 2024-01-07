09355389dfb6ab7b4c95bcffb21768fa294083fd
Jacob Rinderud <jacobrinderud@gmail.com>
2024-01-07 14:10:09 +0100

    part 1 complete

--- 2023/src/9/main.c
+++ 2023/src/9/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <ctype.h>
 
 typedef struct
@@ -7,13 +8,14 @@ typedef struct
     char c;
 } size_char_t;
 
-size_char_t read_numbers(long unsigned *nums, size_t N)
+size_char_t read_numbers(long signed *nums, size_t N)
 {
     size_t i;
-    int c;
-    long unsigned new;
+    int c, sign;
+    long signed new;
 
     new = 0;
+    sign = 1;
     c = '?';
     i = 0;
     while (c != '\n' && c != '@' && i < N)
@@ -22,13 +24,16 @@ size_char_t read_numbers(long unsigned *nums, size_t N)
         switch (c)
         {
         case EOF:
-            // printf("EOF\n");
             c = '@';
         case '\n':
         case ' ':
-            nums[i] = new;
+            nums[i] = new*sign;
             i++;
             new = 0;
+            sign = 1;
+            break;
+        case '-':
+            sign = -1;
             break;
 
         default:
@@ -41,44 +46,36 @@ size_char_t read_numbers(long unsigned *nums, size_t N)
         }
     }
 
-    /*
-    if (i > 0)
-    {
-        printf("last num = %lu\n", nums[i - 1]);
-    }
-    */
-
     size_char_t ret = {i, c};
     return ret;
 }
 
-long unsigned sum(long unsigned *nums, size_t cols)
+long signed allzro(long signed *nums, size_t cols)
 {
-    long unsigned sum = 0;
+    long signed sum = 0;
     for (size_t i = 0; i < cols; i++)
     {
-        sum += nums[i];
+        sum += abs(nums[i]);
     }
     return sum;
 }
 
-long unsigned recur(long unsigned *nums, size_t cols)
+long signed recur(long signed *nums, size_t cols)
 {
     size_t i;
-    /*for (i = 0; i < cols; i++)
+    for (i = 0; i < cols; i++)
     {
-        printf("%lu ", nums[i]);
+        printf("%ld ", nums[i]);
     }
     printf("\n");
-*/
-    if (cols <= 1 || sum(nums, cols) == 0)
+
+    if (cols <= 1 || allzro(nums, cols) == 0)
     {
-        // printf("returning\n\n");
         return 0;
     }
 
-    long unsigned *newrow;
-    long unsigned a, b, diff;
+    long signed *newrow;
+    long signed a, b, diff;
 
     newrow = calloc((cols - 1), sizeof(*newrow));
 
@@ -88,16 +85,16 @@ long unsigned recur(long unsigned *nums, size_t cols)
         b = a;
         a = nums[i];
         diff = a - b;
-        // printf("%lu - %lu = %lu\n", a, b, diff);
+        printf("%ld - %ld = %ld\n", a, b, diff);
 
         newrow[i - 1] = diff;
     }
 
-    long unsigned newdiff, newnewdiff;
+    long signed newdiff, newnewdiff;
     newdiff = recur(newrow, cols - 1);
     newnewdiff = newdiff + nums[cols - 1];
 
-    //printf("%lu + %lu = %lu\n", nums[cols - 1], newdiff, newnewdiff);
+    printf("%ld + %ld = %ld\n", nums[cols - 1], newdiff, newnewdiff);
 
     free(newrow);
     return newnewdiff;
@@ -106,8 +103,8 @@ long unsigned recur(long unsigned *nums, size_t cols)
 int main(void)
 {
     size_t cols, N, i;
-    long unsigned sum, newdiff;
-    long unsigned *nums;
+    long signed sum, newdiff;
+    long signed *nums;
 
     N = 25;
     nums = calloc(N, sizeof(*nums));
@@ -119,15 +116,14 @@ int main(void)
     {
         ret = read_numbers(nums, N);
         cols = ret.i;
-        // printf("%lu\n", nums[cols-1]);
 
         newdiff = recur(nums, cols);
-        printf("%zu: %lu\n",i, newdiff);
+        printf("%zu: %ld\n", i, newdiff);
         sum += newdiff;
         i++;
     }
 
-    printf("total = %lu", sum);
+    printf("total = %ld", sum);
 
     //! Frees
     free(nums);
