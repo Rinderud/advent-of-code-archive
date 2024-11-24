6bd21938d9ae7b641152521e91682b9b841dc67e
Jacob Rinderud <jacobrinderud@gmail.com>
2024-11-24 17:15:37 +0100

    Complete template.

--- 2024/src/template.py
+++ 2024/src/template.py
@@ -1,14 +1,29 @@
 import structlog
 
+
 if __name__ == "__main__":
     DAY = "01"
+    DEBUG = True
+    PART = 1
 
-    log = structlog.get_logger()
-    log.info(f"Day {DAY}!")
+    log = structlog.get_logger(DAY=DAY, PART=PART)
+
+    log.info(f"Day {DAY}, part {PART}!")
 
     input: list[str]
-    with open(f"./data/input_{DAY}.txt") as f:
+    with open(
+        f"./data/input" + f"_{DAY}{'_DEBUG' if DEBUG else ''}" + f"_{PART}" + ".txt",
+        mode="r",
+    ) as f:
         input = f.read().split("\n")
 
     for i, line in enumerate(input):
         log.debug(f"{i}: {line}")
+
+    log.info("Writing to output file")
+    with open(
+        f"./data/output" + f"_{DAY}{'_DEBUG' if DEBUG else ''}" + f"_{PART}" + ".txt",
+        mode="w",
+    ) as f:
+        for row in input:
+            f.write(row + "\n")
